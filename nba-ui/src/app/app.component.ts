9e1780b390d87d4a93c98de2fd1267a23cec7370
synfm@126.com <synfm@126.com>
2023-01-09 23:30:10 -0600

    Small fixes

--- nba-ui/src/app/app.component.ts
+++ nba-ui/src/app/app.component.ts
@@ -17,21 +17,34 @@ export class AppComponent implements OnInit {
   src = ''
   playingIdx = 0
 
+  infoTitle = ''
+  infoDescription = ''
+
+  loadingMatches = true
+
   constructor(private http: HttpClient, public sanitizer: DomSanitizer) { }
 
   ngOnInit() {
+    this.loadingMatches = true
     this.http.get<any>(environment.urlPrefix + 'matches').subscribe(res => {
+      this.loadingMatches = false
       if (res.data) {
         this.matches = res.data
       }
-
-      // $("#errorModal").modal('show')
       
       if (this.matches.length >= 1) {
         this.loadStream(0)
+      } else {
+        this.infoTitle = '未找到比赛'
+        this.infoDescription = '当前没有比赛，请稍后再试。'
+        $("#infoModal").modal('show')
       }
+
     }, error => {
-      alert(error)
+      this.loadingMatches = false
+      this.infoTitle = '系统错误'
+      this.infoDescription = error
+      $("#infoModal").modal('show')
     })
   }
 
@@ -41,9 +54,15 @@ export class AppComponent implements OnInit {
     this.http.get<any>(environment.urlPrefix + 'matches/' + url).subscribe(res => {
       if (res.embed) {
         this.src = res.embed
+      } else {
+        this.infoTitle = '系统错误'
+        this.infoDescription = '未找到embed地址'
+        $("#infoModal").modal('show')
       }
     }, error => {
-      alert(error)
+      this.infoTitle = '系统错误'
+      this.infoDescription = error
+      $("#infoModal").modal('show')
     })
   }
 
