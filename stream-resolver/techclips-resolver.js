bddfc530185dd1914145af861757b3e3bf9d3800
synfm@126.com <synfm@126.com>
2023-04-19 22:38:38 -0500

    Update techclips-resolver.js

--- stream-resolver/techclips-resolver.js
+++ stream-resolver/techclips-resolver.js
@@ -7,12 +7,17 @@ async function resolve(result, teamName1, teamName2) {
   console.log('Resolving techClips')
   try {
     let res = await axios.get('https://techclips.net/schedule/nbastreams/')
-
     // Find url
     const matchRegex = /href="\/(202.*?)".*h3.*?>(.*?)vs(.*?)</g
-    const matches = res.data.matchAll(matchRegex)
+    const matches = res.data.replace(/(\r\n|\n|\r)/gm, "").matchAll(matchRegex)
+
+    console.log(1)
     
     for (const match of matches) {
+      console.log(2)
+      console.log(match[1])
+      console.log(match[2])
+      console.log(match[3])
       let teamRes1 = match[2].toLowerCase().trim()
       let teamRes2 = match[3].toLowerCase().trim()
 
