13456952f295411ad3e3d21b9228e1c671afa8d5
synfm@126.com <synfm@126.com>
2023-04-20 08:56:12 -0500

    Fix tech clips

--- stream-resolver/techclips-resolver.js
+++ stream-resolver/techclips-resolver.js
@@ -8,18 +8,15 @@ async function resolve(result, teamName1, teamName2) {
   try {
     let res = await axios.get('https://techclips.net/schedule/nbastreams/')
     // Find url
-    const matchRegex = /href="\/(202.*?)".*h3.*?>(.*?)vs(.*?)</g
-    const matches = res.data.replace(/(\r\n|\n|\r)/gm, "").matchAll(matchRegex)
-
-    console.log(1)
+    const matchRegex = /href="\/(202.*?)".*?2">(.*?)<\//g
+    const matches = res.data.replace(/(\r\n|\n|\r)/gm, '').matchAll(matchRegex)
     
     for (const match of matches) {
-      console.log(2)
-      console.log(match[1])
-      console.log(match[2])
-      console.log(match[3])
-      let teamRes1 = match[2].toLowerCase().trim()
-      let teamRes2 = match[3].toLowerCase().trim()
+      let teams = match[2].split("vs")
+      if (teams.length != 2) continue
+      
+      let teamRes1 = teams[0].toLowerCase().replace(/series/g, '').trim()
+      let teamRes2 = teams[1].toLowerCase().replace(/series/g, '').trim()
 
       if ((teamName1.toLowerCase().includes(teamRes1) && teamName2.toLowerCase().includes(teamRes2) ) || (
         teamName1.toLowerCase().includes(teamRes2) && teamName2.toLowerCase().includes(teamRes1))) {
